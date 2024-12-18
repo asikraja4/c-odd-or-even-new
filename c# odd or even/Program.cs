be02663ab4bba300eb9520cd171d8cdd3fdbb024
Abdul asik raja <asikraja2001@gmail.com>
2024-12-18 22:14:05 +0530

    Add project files.

---
+++ c# odd or even/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c__odd_or_even
+{
+        class Program
+        {
+            static void Main()
+            {
+                Console.WriteLine("Enter a number:");
+                int number = int.Parse(Console.ReadLine());
+
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine($"{number} is even.");
+                }
+                else
+                {
+                    Console.WriteLine($"{number} is odd.");
+                }
+            }
+        }
+}
---
+++ c# odd or even/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("c# odd or even")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("c# odd or even")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4f3e1e9a-06d8-4227-90ca-f3d52a7f6da7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
